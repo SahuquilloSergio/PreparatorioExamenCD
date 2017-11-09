d24eb201d50b076ed5e5895b7626fb7c6b0b7591
abrandarizdominguez <abrandarizdominguez@victoria9.danielcastelao.org>
2017-11-09 09:07:37 +0100

    Ajuste clase Pedir

--- src/preparatorioexamencd/Pedir.java
+++ src/preparatorioexamencd/Pedir.java
@@ -4,26 +4,18 @@ package preparatorioexamencd;
  * @author Sergio "Sauky" Sahuquillo & abrandarizdominguez
  */
 public class Pedir {
-    
-private String nombre;
-
-//Constructor por defecto
-public Pedir(){
-    
-}
-//Constructor por parametros
-public Pedir(String nom){
+    private String nombre;
+    //Constructor por defecto
+    public Pedir(){    
+    }
+    //Constructor por parametros
+    public Pedir(String nom){
     nom = nombre;
-}
-
-//Metodos de acceso
-public void setNombre(String nombre){
-    
-}
-public String getNombre(){
+    }
+    //Metodos de acceso
+    public void setNombre(String nombre){
+    }
+    public String getNombre(){
     return nombre;
-}
-
-
-
+    }
 }
