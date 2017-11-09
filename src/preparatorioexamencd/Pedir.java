a1d361fc218e20648a2c1a2b7d8187e04471b7df
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-11-09 09:16:48 +0100

    Commit 4

--- src/preparatorioexamencd/Pedir.java
+++ src/preparatorioexamencd/Pedir.java
@@ -4,26 +4,18 @@ package preparatorioexamencd;
  * @author Sergio "Sauky" Sahuquillo & abrandarizdominguez
  */
 public class Pedir {
-    
-private String nombre;
-
-//Constructor por defecto
-public Pedir(){
-    
-}
-//Constructor por parametros
-public Pedir(String nombre){
-    this.nombre = nombre;
-}
-
-//Metodos de acceso
-public void setNombre(String nombre){
-    
-}
-public String getNombre(){
+    private String nombre;
+    //Constructor por defecto
+    public Pedir(){    
+    }
+    //Constructor por parametros
+    public Pedir(String nom){
+    nom = nombre;
+    }
+    //Metodos de acceso
+    public void setNombre(String nombre){
+    }
+    public String getNombre(){
     return nombre;
-}
-
-
-
+    }
 }
