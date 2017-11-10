5d51d32af57488ca13785929b38b249a993c7875
abrandarizdominguez <abrandarizdominguez@victoria9.danielcastelao.org>
2017-11-10 08:57:27 +0100

    System de timo

--- src/preparatorioexamencd/PreparatorioExamenCD.java
+++ src/preparatorioexamencd/PreparatorioExamenCD.java
@@ -20,6 +20,7 @@ public class PreparatorioExamenCD {
         Mostrar mos = new Mostrar(nombre);
         mos.saludar(nombre);
         mos.toString();
+        System.out.println("Menudo timo, devuelvanme mi dinero");
     }
     
 }
