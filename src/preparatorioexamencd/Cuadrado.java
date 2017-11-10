8d4a23da521865dc916ba4aef1f1336008507654
abrandarizdominguez <abrandarizdominguez@victoria9.danielcastelao.org>
2017-11-10 09:19:05 +0100

    Parida2CommitC

---
+++ src/preparatorioexamencd/Cuadrado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preparatorioexamencd;
+
+/**
+ *
+ * @author abrandarizdominguez
+ */
+public class Cuadrado {
+    public void parida1 (){
+        System.out.println("Parida2 Commit C");
+    }
+}
